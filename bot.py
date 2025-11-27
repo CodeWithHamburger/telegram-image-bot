ae6c9c15568491aeeb0ad323a0d0d3cbb8d7e4ea
CodeWithHamburger <serg.schamin@gmail.com>
2025-11-27 20:45:11 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,36 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.filters import Command
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()
-bot = Bot(os.getenv("BOT_TOKEN"))
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.answer("Привет! Это мой первый Telegram бот 🤖")
+    await message.answer("Привет! Напиши описание изображения, и я сгенерирую картинку 🎨🤖")
 
 @dp.message()
-async def echo(message: Message):
-    await message.answer(f"Ты написал: {message.text}")
+async def generate_image(message: Message):
+    prompt = message.text
+    await message.answer(f"Генерирую изображение, подожди 5-10 секунд... 🔄🤗")
+
+    img = client.images.generate(
+        model="gpt-image-1",
+        prompt=prompt,
+        size="1024x1024"
+    )
+
+    await message.answer_photo(img.data[0].url, caption="Изображение готово!😊")
 
 if __name__ == "__main__":
-    dp.run_polling(bot)
+    import asyncio
+    asyncio.run(dp.start_polling(bot))
